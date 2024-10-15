01e426e17e044179ab33b810dbcdf00648878aae
vladyslav tsikhotskyi <81439835+vlad14710@users.noreply.github.com>
2024-10-15 12:19:11 +0300

    in

--- src/test/java/SortingAppTest.java
+++ src/test/java/SortingAppTest.java
@@ -1,27 +1,35 @@
 import org.example.SortingApp;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(Parameterized.class)
 public class SortingAppTest {
+
+    private int[] input;
+    private int[] expected;
+
+    public SortingAppTest(int[] input, int[] expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{5, 3, 8, 1, 2}, new int[]{1, 2, 3, 5, 8}},
+                {new int[]{}, new int[]{}} ,
+                {new int[]{1}, new int[]{1}}
+        });
+    }
 
     @Test
     public void testSorting() {
-        int[] input = {5, 3, 8, 1, 2};
-        int[] expected = {1, 2, 3, 5, 8};
-        assertArrayEquals(expected, SortingApp.sort(input));
-    }
-
-    @Test
-    public void testEmptyArray() {
-        int[] input = {};
-        int[] expected = {};
-        assertArrayEquals(expected, SortingApp.sort(input));
-    }
-
-    @Test
-    public void testSingleElement() {
-        int[] input = {1};
-        int[] expected = {1};
         assertArrayEquals(expected, SortingApp.sort(input));
     }
 }
